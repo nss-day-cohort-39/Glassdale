0a62d9134f5fda14cabeb623f43ec0bb8c6b6281
Steve Brownlee <chortlehoort@gmail.com>
2020-03-24 10:26:24 -0500

    Criminals now sorted by last name

--- scripts/criminals/CriminalProvider.js
+++ scripts/criminals/CriminalProvider.js
@@ -1,6 +1,21 @@
 let criminals = []
 
-export const useCriminals = () => criminals.slice()
+export const useCriminals = () => {
+    // Sort by criminal last name before the copy happens
+    criminals.sort(
+        (currentThing, nextThing) => {
+            // Get the last name on both objects
+            const [currFirstName, currLastName] = currentThing.name.split(" ")
+            const [nextFirstName, nextLastName] = nextThing.name.split(" ")
+
+            if (currLastName < nextLastName) { return -1; }
+            if (currLastName > nextLastName) { return 1; }
+            return 0;
+        }
+    )
+
+    return criminals.slice()
+}
 
 export const getCriminals = () => {
     /*
@@ -8,9 +23,9 @@ export const getCriminals = () => {
         Make sure the last `then()` sets the local `Criminals`
         variable to what is in the response from the API.
     */
-   return fetch("https://criminals.glassdale.us/criminals")
-    .then(response => response.json())
-    .then(parsedCriminals => {
-        criminals = parsedCriminals
-    })
+    return fetch("https://criminals.glassdale.us/criminals")
+        .then(response => response.json())
+        .then(parsedCriminals => {
+            criminals = parsedCriminals
+        })
 }
